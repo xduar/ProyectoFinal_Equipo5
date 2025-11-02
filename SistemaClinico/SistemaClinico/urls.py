32f345e38d7c73c15e689aa40bf7a86f802e6305
oscar jonatan vasquez rivera <vasquezriveraoscarjonatan@gmail.com>
2025-11-02 05:32:17 -0600

    Actualiza rutas principales en urls.py

--- SistemaClinico/SistemaClinico/urls.py
+++ SistemaClinico/SistemaClinico/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('clinica.urls')),
+    path('login/', LoginView.as_view(template_name='clinica/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout')
 ]
