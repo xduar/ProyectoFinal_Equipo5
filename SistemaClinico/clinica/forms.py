cb9843c3d135d6d46efb9a91b38483a0d18818d6
brayanjosuemartinez41-wq <brayanjosuemartinez41@gmail.com>
2025-11-02 21:08:55 -0600

    Define formularios clínicos en forms.py: vincula modelos con campos personalizados y mejora validación en el frontend

--- SistemaClinico/clinica/forms.py
+++ SistemaClinico/clinica/forms.py
@@ -1,5 +1,32 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User, Group
 from .models import Paciente
+
+class RegistroUsuarioForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+            # Añadir el usuario al grupo de Pacientes
+            grupo_pacientes = Group.objects.get_or_create(name='Pacientes')[0]
+            user.groups.add(grupo_pacientes)
+        return user
 
 
 class PacienteForm(forms.ModelForm):
