b6d43c78723fd5602f2d2956eebaa81aef1aac48
eduar <tucorreo@example.com>
2025-11-04 18:35:26 -0600

    se agrego configuracion a settings.py

--- SistemaClinico/SistemaClinico/settings.py
+++ SistemaClinico/SistemaClinico/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,3 +137,11 @@ LOGOUT_REDIRECT_URL = 'clinica:inicio'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'clinica/static'), )
+
+# para desarrollo, indicar donde buscar archivos estaticos en cada app
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
