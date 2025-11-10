b6f916eb281ccb028c8e3f967d45949b5394758b
eduar <tucorreo@example.com>
2025-11-10 12:54:34 -0600

    Add render.yaml for deployment and update build.sh

--- SistemaClinico/build.sh
+++ SistemaClinico/build.sh
@@ -2,7 +2,8 @@
 
 set -o errexit
 
-pip install -r requirements.txt
+pip install -r ../requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate --no-input
 
-python manage.py collectstatic --noinput
-python manage.py migrate
+gunicorn SistemaClinico.wsgi:application
