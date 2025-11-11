c030964daee532393f3e2d3906744868ba43916a
eduar <tucorreo@example.com>
2025-11-10 23:04:47 -0600

    preparando para subirlo a render

--- SistemaClinico/SistemaClinico/settings.py
+++ SistemaClinico/SistemaClinico/settings.py
@@ -88,7 +88,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Archivos estáticos
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static']
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
