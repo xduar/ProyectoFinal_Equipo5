e91c3945c32eebc2cc41827e3620db778d22975c
eduar <tucorreo@example.com>
2025-11-10 23:02:48 -0600

    preparando para subirlo a render

--- SistemaClinico/SistemaClinico/settings.py
+++ SistemaClinico/SistemaClinico/settings.py
@@ -68,7 +68,7 @@ if DB_ENGINE:
     # Si hay variables de entorno, usar esas (producción)
     DATABASES = {
         'default': {
-            'ENGINE': DB_ENGINE,
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.getenv('DB_NAME'),
             'USER': os.getenv('DB_USER'),
             'PASSWORD': os.getenv('DB_PASSWORD'),
